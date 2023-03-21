573d60b1337234e8ed4ff8ccef2db8b1f4de9aa2
ToasterXE <114070865+ToasterXE@users.noreply.github.com>
2023-03-21 18:34:49 +0100

    e

--- a3.py
+++ a3.py
@@ -10,6 +10,17 @@ def sort(pancakes):
         newpancakes.append(pancakes[i])
     pancakes_global = newpancakes
 
+def sort_wbase(pancakes,base = 0):
+    global pancakes_global
+    newpancakes = []
+    print(base)
+    for i in range(len(pancakes)-base-2,-1,-1):
+        newpancakes.append(pancakes[i])
+
+    for i in range(base-2,len(pancakes)):
+        newpancakes.append(pancakes[i])
+    pancakes_global = newpancakes
+
 def sorted(pancakes):
     previous = pancakes[-1]
     for i in range(len(pancakes)-1,-1,-1):
@@ -26,22 +37,27 @@ class struct():
         self.length = 1
         self.interruption = pancakes_global[index+1]
         self.last = self.head
+        self.dir = False
+        self.start = index
         if self.head > pancakes_global[index+2]:
             self.dir = True
-        else:
-            self.dir = False
         for i in range(index + 2, len(pancakes_global)):
             if bool(self.last > pancakes_global[i]) == self.dir:
+                structmap[i] = "x"
                 self.length += 1
                 self.last = pancakes_global[i]
             else:
                 break
-        self.extreme = min(int(self.head), int(pancakes_global[self.head + self.length]))
-        if 
+        print(f"head = {self.head} len = {self.length} index = {index+self.length}")
+        self.extreme = min(int(self.head), int(pancakes_global[int(index + self.length)]))
         if self.length >= 3:
             valid_structs.append(self)
 
-    
+    def solve(self):
+        print(pancakes_global)
+        print(f"len={len(pancakes_global)} start = {self.start}")
+        sort_wbase(pancakes_global,len(pancakes_global)-1-(self.start+1))
+        print(pancakes_global)
 
 def find_structures(pancakes):
     structmap = pancakes.copy()
@@ -49,7 +65,15 @@ def find_structures(pancakes):
         if structmap[i] != "x":
             struct(i, structmap)
     print(valid_structs)
+    for structe in valid_structs:
+        structe.solve()
+    # print(pancakes)
+    # sort(pancakes)
+    # print(pancakes_global)
 
+    # sort_wbase(pancakes,4)
+    # print(pancakes_global)
+    
 def main():
     with open(("pancake0.txt"),"r",encoding='utf8') as datei:
         original_size = datei.readline().strip()
