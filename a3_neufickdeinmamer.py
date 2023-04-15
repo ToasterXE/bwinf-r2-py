dc0f937f152c3b74bb3940767eae3fee47b6bcc2
ToasterXE <114070865+ToasterXE@users.noreply.github.com>
2023-04-15 13:54:34 +0200

    e

--- a3_neufickdeinmamer.py
+++ a3_neufickdeinmamer.py
@@ -1,13 +1,15 @@
 import math
-import random, os
+import random, os, time, sys
 from threading import Thread
 
 cores = os.cpu_count()
 if not cores:
     cores = 4
 
+solutions = []
 pancakes_global = []
 og_size = 0
+solved = []
 
 def genstack(len):
     stack = []
@@ -18,9 +20,11 @@ def genstack(len):
         stack.append(pancake)
 
 counter = 0
-def sort(pancakes,base = 0):
-    global counter
+def sort(pancakes,base = 0,liste=0):
+    global counter, solutions
     counter += 1
+    # if counter == numofsolution//10:
+    #     print("10%...")
     newpancakes = []
     # print(f"base: {base}")
     for i in range(len(pancakes)-base-2,-1,-1):
@@ -28,38 +32,60 @@ def sort(pancakes,base = 0):
 
     for i in range(len(pancakes)-base,len(pancakes)):
         newpancakes.append(pancakes[i])
-    
     if sorted(newpancakes)and len(newpancakes)>1:
-        pass
-        # print(f"sorted {newpancakes}")
+        solved[len(pancakes)].append(pancakes)
+        # print(f"sorted {newpancakes} schritte: {liste}")
+        solutions.append([newpancakes,liste])
     return(newpancakes)
 
 def sorted(pancakes):
-    previous = pancakes[-1]
+    previous = int(pancakes[-1])
     for i in range(len(pancakes)-1,-1,-1):
-        current = pancakes[i]
+        current = int(pancakes[i])
         if current > previous:
             return False
-        previous = current
+        previous = current  
+    print(solved)
+    
     return True
 
+def normalize(stack):
+    nstack= stack.copy()
+    for i in range(0,len(stack)):
+        max_e = max(stack)
+        index = stack.index(max_e)
+        stack[index] = 0
+        nstack[index] = len(stack)-i
+    return nstack
 
-def recurse(stack):   #es gibt mehr lösungswege als lösungen; lösungswege rechnet man aus mit
-    for i in range(0, len(stack)):
-        nstack = stack.copy()
-        nstack = sort(nstack,i)
-        print(f"stack:{stack}nstack:{nstack}")
-        if len(nstack)>1:
-            recurse(nstack)
+
+def recurse(stack,liste):   #es gibt mehr lösungswege als lösungen; lösungswege rechnet man aus mit get_numofsolutions()
+    if stack not in solved[len(stack)]:
+        for i in range(0, len(stack)):
+            if stack not in solved[len(stack)]:
+                lister = liste.copy()
+                lister.append(i)
+                nstack = stack.copy()
+                nstack = sort(nstack,i,lister)
+                # print(f"stack:{stack}nstack:{nstack}")
+                if len(nstack)>1:
+                    recurse(nstack,lister)
  
-
-def beans(startdata):
+def start_sort(startdata):
     for start in startdata:
         pancakes = pancakes_global.copy()
         pancakes = sort(pancakes,start-1)
-        print(counter)
-        recurse(pancakes)
+        recurse(pancakes,[start-1])
 
+def get_longest():
+    print("counting pancake stacks...")
+    longest = solutions[0][0]
+    solution = solutions[0][1]
+    for stack in solutions:
+        if len(stack[0]) > len(longest):
+            longest = stack[0]
+            solution = stack[1]
+    return(longest, solution)
 
 def main():
     global og_size
@@ -67,7 +93,11 @@ def main():
         og_size = int(datei.readline().strip())
         for line in datei:
             pancakes_global.append(line.strip())
+            solved.append([])
+    print("calculating time...")
+    print(f"estimated time: {calculate_etime()}")
     execute_threads(get_threads())
+    print(get_longest())
 
 def get_numofsolutions(size):
     numofsolutions = 0
@@ -79,7 +109,9 @@ def get_numofsolutions(size):
 
     return numofsolutions
     
+
 def get_threads():
+    print("creating threads...")
     threads = []
     for i in range(0,og_size):
         if i >= cores:
@@ -89,13 +121,27 @@ def get_threads():
     return threads
 
 def execute_threads(threadlist):
+    print("executing threads...")
     exe_threads = []
     for data in threadlist:
-        exe_threads.append(Thread(target=beans, args=(data,)))
+        exe_threads.append(Thread(target=start_sort, args=(data,)))
     for e in exe_threads:
         e.start()
+    print("eating pancakes...")
     for e in exe_threads:
         e.join()
+
+def calculate_etime():
+    starttime = time.time()
+    for i in range(0,1000000):
+        sort(pancakes_global.copy(),random.randint(0,og_size))
+    endtime = time.time()
+    etime100 = endtime-starttime
+    etime = (etime100 * get_numofsolutions(og_size)) / 1000000
+    return f"{etime//60}m {int(etime%60)}s"
+numofsolution = get_numofsolutions(og_size)
 # main()
-print(get_numofsolutions(5))
+print(normalize([8, 2, 11]))
+# print(sorted(['1', '11', '4', '5', '7', '9']))
+# print(get_numofsolutions(5))
 # print(counter)
